f95654283c3958c5a1a81698ed27e2e509a7e2a8
Gregory Amaefule <Amaefulegregory@gmail.com>
2022-08-17 17:10:17 +0100

    completed menu component of the UI

--- Part_One/main.js
+++ Part_One/main.js
@@ -1,6 +1,20 @@
 const mobileNav = document.querySelector('.mobileHeader')
 const cancelIcon = document.querySelector('#cancelIcon')
 const hamBurgerMenu = document.querySelector('.hamBurger')
+const containers = document.querySelectorAll('.container')
+const buttons = document.querySelectorAll('.container div div button')
+const headers = document.querySelectorAll('.container div h3')
+
+
+// function handleMouseOver(){
+//     for (const button of buttons){
+//         button.style.display = 'block'
+//     }
+//     for (const header of headers){
+//         header.style.color = 'orange'
+//     }
+
+// }
 
 function handleHamburgerClick(){
     mobileNav.style.display = 'block';
@@ -10,3 +24,7 @@ hamBurgerMenu.addEventListener('click', handleHamburgerClick)
 cancelIcon.addEventListener('click', ()=>{
     mobileNav.style.display = 'none'
 })
+
+// for (const container of containers){
+//     container.addEventListener('mouseover', handleMouseOver)
+// }
